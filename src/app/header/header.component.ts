a065f3cbc4539e171a35b59ea33cbe63bea8ac52
JaimeNM0 <navarro.m.jai@gmail.com>
2024-02-20 17:49:03 +0100

    Creación del toolbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
